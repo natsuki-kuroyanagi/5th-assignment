ecad26c7ed2e4c86902cc81e8023ceb4d0f239bb
natsuki kuroyanagi <h.t.spece6725@gmail.com>
2023-10-01 15:24:19 +0900

    プロジェクトをセットアップ

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Student {
+
+    private int number;
+
+    private int age;
+
+    private String name;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Student(int number, int age, String name) {
+        this.number = number;
+        this.age = age;
+        this.name = name;
+
+
+    }
+}
